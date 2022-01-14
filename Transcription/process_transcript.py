da34a17292cc10d9af15f04502baa2bc5a3053ef
JinitSan <jinitsanghvi@gmail.com>
2022-01-14 15:51:57 +0530

    added synthetic timestamps

--- Transcription/process_transcript.py
+++ Transcription/process_transcript.py
@@ -13,9 +13,31 @@ def readj(name):
     sentences = {}
     transcript = data['combinedRecognizedPhrases'][0]['display']
 
-    for i in phrases:
-        start_time = i['offsetInTicks']//(10**7)
-        sentences[start_time] = i['nBest'][0]['display'] 
+    for i in range(len(phrases)-1):
+        start_time = phrases[i]['offsetInTicks']//(10**7)
+        l_sentences = phrases[i]['nBest'][0]['display'].split(".")[:-1]
+        if len(l_sentences)>1:
+            end_time = phrases[i+1]['offsetInTicks']//(10**7)
+            delta = (end_time-start_time)/len(l_sentences)
+            for j in l_sentences:
+                sentences[start_time] = j + "."
+                start_time = start_time+delta
+        else:
+            sentences[start_time] = phrases[i]['nBest'][0]['display']
+    l_sentences = phrases[-1]['nBest'][0]['display'].split(".")[:-1]
+    start_time = phrases[-1]['offsetInTicks']//(10**7)
+    if len(l_sentences)>1:
+        delta = (phrases[-1]['durationInTicks']//(10**7))/len(l_sentences)
+        for j in l_sentences:
+            sentences[start_time] = j + "."
+            start_time = start_time+delta
+    else:
+        sentences[start_time] = phrases[i]['nBest'][0]['display']
     #find_sentence_for_frame(30,70,sentences)
+    print(sentences)
     return sentences
+
+if __name__=="__main__":
+    readj("transcript.json")
+    
     
