84a85626b72bf6f2ea5d9a8bfb00565cbccb1432
Shruti <shrutisusan055@gmail.com>
2022-01-19 11:29:38 +0530

    Rouge eval for text added

--- geteval.py
+++ geteval.py
@@ -1,4 +1,5 @@
 import numpy as np
+from rouge_score import rouge_scorer
 #user_sum - a numpy array of shape(user, frame_selected)
 def get_f1_score(pred_sum,user_sum,f1_type='max'):
 	max_val=max(len(pred_sum),user_sum.shape[1])
@@ -20,3 +21,16 @@ def get_f1_score(pred_sum,user_sum,f1_type='max'):
 		return max(f_scores)
 	else:
 		return sum(f_scores)/len(f_scores) #avg
+#summary- list of strings
+#ref can be a string/list
+def test_text(summary,reference):
+		summ='.'.join(summary)
+		reference='.'.join(reference)
+		scores=rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+		score = scores.score(summ,reference)
+		print(score)
+		return score
+
+
+if __name__=="__main__":
+		test_text(['this is her','what'],['this her','ok'])
